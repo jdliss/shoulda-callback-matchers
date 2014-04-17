663c01c0d945490982a8146a6c3fcc054e12f3cf
beatrichartz <beat.richartz@gmail.com>
2014-04-17 10:36:57 +0100

    . refactor rails version helper

--- lib/shoulda/callback/matchers/rails_version_helper.rb
+++ lib/shoulda/callback/matchers/rails_version_helper.rb
@@ -4,19 +4,41 @@ module Shoulda
     module Matchers
       module RailsVersionHelper
         def rails_4_1?
-          rails_4? && ActiveRecord::VERSION::MINOR == 1
+          rails_4? && minor_version_equals?(1)
         end
   
         def rails_4_0?
-          rails_4? && ActiveRecord::VERSION::MINOR == 0
+          rails_4? && vminor_version_equals?(0)
         end
   
         def rails_4?
-          ActiveRecord::VERSION::MAJOR == 4
+          major_version_equals? 4
         end
   
         def rails_3?
-          ActiveRecord::VERSION::MAJOR == 3
+          major_version_equals? 3
+        end
+        
+        private
+        
+        def major_version_equals? number
+          if active_record?
+            ActiveRecord::VERSION::MAJOR == number
+          else
+            ActiveModel::VERSION::MAJOR == number
+          end
+        end
+        
+        def minor_version_equals? number
+          if active_record?
+            ActiveRecord::VERSION::MINOR == number
+          else
+            ActiveModel::VERSION::MINOR == number
+          end
+        end
+        
+        def active_record?
+          defined?(::ActiveRecord)
         end
       end
     end
