66b7c9eeb4607589af72df226617f897ea835a69
beatrichartz <beat.richartz@gmail.com>
2014-04-17 10:36:31 +0100

    . cleanup spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,20 +1,21 @@
-# Create Rails environment based on the version given from Appraisal
-TESTAPP_ROOT = File.join(File.dirname(__FILE__), '..', 'tmp', 'aruba', 'testapp')
-FileUtils.rm_rf(TESTAPP_ROOT) if File.exists?(TESTAPP_ROOT)
-`rails new #{TESTAPP_ROOT}`
+TESTAPP_ROOT = Pathname.new File.expand_path('../tmp/aruba/testapp', __FILE__)
+FileUtils.rm_rf TESTAPP_ROOT if File.exists? TESTAPP_ROOT
 
 ENV['RAILS_ENV'] = 'test'
-ENV['BUNDLE_GEMFILE'] ||= TESTAPP_ROOT + '/Gemfile'
+ENV['BUNDLE_GEMFILE'] ||= TESTAPP_ROOT.join('Gemfile')
 
-require "#{TESTAPP_ROOT}/config/environment"
+`rails new #{TESTAPP_ROOT}`
+
+require TESTAPP_ROOT.join('config', 'environment')
 require 'shoulda-callback-matchers'
 require 'rspec/rails'
 
-PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')).freeze
+PROJECT_ROOT = Pathname.new File.expand_path('../..', __FILE__)
+$LOAD_PATH << PROJECT_ROOT.join('lib')
 
-$LOAD_PATH << File.join(PROJECT_ROOT, 'lib')
-
-Dir[File.join(PROJECT_ROOT, 'spec', 'support', '**', '*.rb')].each { |file| require(file) }
+Dir[PROJECT_ROOT.join('spec', 'support', '**', '*.rb')].each do |file|
+  require file
+end
 
 # Run the migrations
 ActiveRecord::Migration.verbose = false
